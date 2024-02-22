ac2b47e8580a8f1847f79b91750993000eca6ed7
Luigi Bolzon <luigibolzon@gmail.com>
2024-02-22 20:08:06 -0300

    Criei as listas também, mas preciso arrumar em arrays e deixar arrumadinho

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Botao from './Components/botao';
 import Formulario from './Components/formulario';
+import Lista from './Components/lista';
 
 
 function App() {
   return (
     <div className="App">
        <Formulario />
+       <Lista /> 
     </div>
   );
 }
